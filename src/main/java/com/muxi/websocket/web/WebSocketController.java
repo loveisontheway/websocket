a3d5bf372d0ee590c5c2058fa5a8403ea58d640e
jiangjialiang <jiangjialiang0746@163.com>
2022-08-11 10:58:18 +0800

    no message

---
+++ src/main/java/com/muxi/websocket/web/WebSocketController.java
@@ -0,0 +1,50 @@
+package com.muxi.websocket.web;
+
+import com.muxi.websocket.service.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+/**
+ * websocket controller
+ *
+ * @author jjl
+ * @date 2022/8/11
+ */
+@Controller
+@RequestMapping("/socket")
+public class WebSocketController {
+
+    @Value("${socket.pwd}")
+    public String password;
+
+    @Autowired
+    private WebSocketServer webSocketServer;
+
+    /**
+     * 手机客户端请求接口
+     *
+     * @param id  发生异常的设备ID
+     * @param pwd 密码（实际开发记得加密）
+     * @throws IOException
+     */
+    @PostMapping(value = "/notice")
+    @ResponseBody
+    public void notice(String id, String pwd) throws IOException {
+        // 密码校验一致（这里举例，实际开发还要有个密码加密的校验的），则进行群发
+        if (password.equals(pwd)) {
+            webSocketServer.broadCastInfo(id);
+        }
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
